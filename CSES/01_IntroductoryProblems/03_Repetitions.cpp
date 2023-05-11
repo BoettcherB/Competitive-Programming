9314a6faec9af9eba2e6f2a3802d4c57aa1ac23d
Brandon Boettcher <brandonboettcher19@gmail.com>
2023-05-11 04:20:25 -0500

    Create 03_Repetitions.cpp

---
+++ CSES/01_IntroductoryProblems/03_Repetitions.cpp
@@ -0,0 +1,39 @@
+/*
+
+You are given a string of length n containing the characters A, C, G, and T.
+Find the length of the longest repetition in this string (the longest substring
+where all characters are equal).
+
+To solve this problem, loop through the characters of the string while keeping
+track of the length of the current repetition and the length of the longest
+repetition.
+
+Constraints:
+ - 1 <= n <= 1,000,000
+
+Example Input:
+ATTCGGGA
+
+Example Output:
+3
+
+*/
+
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::string str;
+    std::cin >> str;
+    int max = 0, cur = 0;
+    char prev = 0;
+    for (char c : str) {
+        cur = (c == prev) ? cur + 1 : 1;
+        max = std::max(cur, max);
+        prev = c;
+    }
+    std::cout << max << '\n';
+}
