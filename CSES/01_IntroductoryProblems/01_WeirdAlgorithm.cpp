649b7d9d617466f365d17722d9d7b44a8da57693
Brandon Boettcher <brandonboettcher19@gmail.com>
2023-05-16 01:03:46 -0500

    Create 01_WeirdAlgorithm.cpp

--- CSES/01_IntroductoryProblems/01_WeirdAlgorithm.cpp
+++ CSES/01_IntroductoryProblems/01_WeirdAlgorithm.cpp
@@ -6,9 +6,6 @@ n is equal to 1:
  - If n is odd, n = 3n + 1
 Print every value of n until n is equal to 1.
 
-The only thing to watch out for in this problem is that n could become very
-large and needs to be stored in a 64 bit number.
-
 Constraints:
  - 1 <= n <= 1,000,000
 
@@ -17,6 +14,9 @@ Example Input:
 
 Example Output:
 3 10 5 16 8 4 2 1
+
+The only thing to watch out for in this problem is that n could become very
+large and needs to be stored in a 64 bit number.
 
 */
 
@@ -31,7 +31,7 @@ int main() {
     std::cin >> n;
     while (n != 1) {
         std::cout << n << ' ';
-        n = (n % 2) ? 3 * n + 1 : n / 2 ;
+        n = (n % 2) ? 3 * n + 1 : n / 2;
     }
     std::cout << n << '\n';
 }
