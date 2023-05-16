d164db1f329a9505e1b83ec930ea8a4ce520df0a
Brandon Boettcher <brandonboettcher19@gmail.com>
2023-05-16 01:18:46 -0500

    Create 11_CoinPiles.cpp

--- CSES/01_IntroductoryProblems/11_CoinPiles.cpp
+++ CSES/01_IntroductoryProblems/11_CoinPiles.cpp
@@ -2,18 +2,10 @@
 
 You have 2 coin piles containing a and b coins. On one move you can take 2
 coins off of one pile and 1 coin off of the other. Is it possible to empty
-both piles? Answer this for t test cases.
-
-If both piles have the same number of coins (a == b), then it is only possible
-to make both piles empty if a and b are divisible by 3. If we take 2 from a and
-1 from b, then 2 from b and 1 from a, a and b will be equal again and we will
-have taken away 3 from both piles. If a != b, we can make them equal by
-continually taking 2 from the larger pile and 1 from the smaller until they are
-equal. However, this is only possible if the larger is less than twice the
-smaller.
+both piles? Answer this for T test cases.
 
 Constraints:
- - 1 <= t <= 100,000
+ - 1 <= T <= 100,000
  - 1 <= a, b <= 1,000,000,000
 
 Example Input:
@@ -26,6 +18,14 @@ Output:
 YES
 NO
 YES
+
+If both piles have the same number of coins (a == b), then it is only possible
+to make both piles empty if a and b are divisible by 3. If we take 2 from a and
+1 from b, then 2 from b and 1 from a, a and b will be equal again and we will
+have taken away 3 from both piles. If a != b, we can make them equal by
+continually taking 2 from the larger pile and 1 from the smaller until they are
+equal. However, this is only possible if the larger is less than twice the
+smaller.
 
 */
 
