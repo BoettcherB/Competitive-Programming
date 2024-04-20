a85cc0273cb0333af7f96c5c85586fbe8552149d
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-04-20 18:15:47 -0500

    Create 02_MissingNumber.cpp

--- CSES/1_IntroductoryProblems/02_MissingNumber.cpp
+++ CSES/1_IntroductoryProblems/02_MissingNumber.cpp
@@ -1,3 +1,24 @@
+/*
+
+You are given an integer n and all numbers 1..n (inclusive) except one.
+Determine which number is missing and print it.
+
+Constraints:
+ - 2 <= n <= 200,000
+
+Example Input:
+5
+2 3 1 5
+
+Example Output:
+4
+
+Solve this problem by setting up a boolean array where arr[i] = false for all
+1 <= i <= n. Then, for every number x we are given, set arr[x] = true. Finally,
+loop through arr and find the index where arr[x] is still false.
+
+*/
+
 #include <iostream>
 
 const int N = 2e5 + 10;
