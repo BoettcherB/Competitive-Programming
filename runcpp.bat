6374f972e92e28f5b7292bc6623daf2f6c6f29be
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-06-08 11:04:54 -0500

    Create runcpp.bat

---
+++ runcpp.bat
@@ -0,0 +1,32 @@
+:: Add the directory containing this file to $PATH
+
+@echo off
+
+:: Make sure a C++ file is provided 
+if "%1"=="" (
+    echo Usage: %0 cppfile.cpp
+    exit /b 1
+)
+
+set CPPFILE=%1
+set EXEFILE=a.exe
+set INPUTFILE=%~dp0in.txt
+
+:: Compile the C++ file
+echo Compiling...
+g++ %CPPFILE% -o %EXEFILE%
+
+:: Check that the compilation was successful
+if %ERRORLEVEL% NEQ 0 (
+    echo Compilation failed.
+    exit /b %ERRORLEVEL%
+)
+echo Compilation Successful
+
+:: Run the C++ executable with input from INPUTFILE
+echo Running...
+echo --------------------------
+type %INPUTFILE% | .\%EXEFILE%
+
+:: clean up the executable
+del %EXEFILE%
