88ccd1feb4dc4195d7192086f30f6f4de7a65739
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-06-11 08:50:28 -0500

    Create A.cpp

---
+++ Codeforces/Codeforces-Round-0943/A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int tc;
+    std::cin >> tc;
+    while (tc--) {
+        int x, max = 0, bestY = 0;
+        std::cin >> x;
+        for (int y = 1; y < x; ++y) {
+            int val = gcd(x, y) + y;
+            if (val > max) {
+                max = val;
+                bestY = y;
+            }
+        }
+        std::cout << bestY << '\n';
+    }
+}
