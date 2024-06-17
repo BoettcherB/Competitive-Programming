6c35e548728740f0606c48270536d559d0f93bca
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-06-17 01:41:02 -0500

    Create template.cpp

--- template.cpp
+++ template.cpp
@@ -36,11 +36,46 @@ struct debug {
 #define pprint(x)
 #endif
 
+const int MOD = 998244353;
+
+struct mint {
+    long long v;
+    mint(long long val = 0) { v = val % MOD + (val < 0) * MOD; }
+    mint& operator+=(const mint o) { if ((v += o.v) > MOD) v -= MOD; return *this; }
+    mint& operator-=(const mint o) { if ((v -= o.v) < 0) v += MOD; return *this; }
+    mint& operator*=(const mint o) { v = v * o.v % MOD; return *this; }
+    mint& operator/=(const mint o) { return *this *= mpow(o, MOD - 2); }
+    mint operator+(const mint o) const { return mint(*this) += o; }
+    mint operator-(const mint o) const { return mint(*this) -= o; }
+    mint operator*(const mint o) const { return mint(*this) *= o; }
+    mint operator/(const mint o) const { return mint(*this) /= o; }
+    friend mint mpow(mint a, long long b) {
+        mint res = 1; while (b) { if (b & 1) res *= a; a *= a; b >>= 1; }
+        return res;
+    }
+    operator long long() const { return v; }
+    friend std::ostream& operator<<(std::ostream& os, const mint& m) {
+        return os << m.v;
+    }
+    friend std::istream& operator>>(std::istream& is, mint& m) {
+        long long val; std::cin >> val; m = mint(val); return is;
+    }
+};
+
+
+
+
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int n;
-    std::cin >> n;
+    int tc;
+    std::cin >> tc;
+    while (tc--) {
+        int n;
+        std::cin >> n;
+        
+    }
 }
