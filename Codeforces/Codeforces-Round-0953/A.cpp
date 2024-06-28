dd55b4499838eed5c93ba40c4d3ca5a7f9714772
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-06-28 13:47:13 -0500

    Create A.cpp

---
+++ Codeforces/Codeforces-Round-0953/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int tc;
+    std::cin >> tc;
+    while (tc--) {
+        int n;
+        std::cin >> n;
+        std::vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            std::cin >> a[i];
+        }
+        int res = *std::max_element(a.begin(), a.end() - 1) + a[n - 1];
+        std::cout << res << '\n';
+    }
+}
